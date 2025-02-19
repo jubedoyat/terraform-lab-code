6e6c20d35210266f1aa099007e1188c9bf15edfe
Juan Bedoya <54643704+jubedoyat@users.noreply.github.com>
2025-02-18 21:42:50 -0500

    Terraform code added

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {}
+
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  type        = string
+  sensitive   = true
+}
+
+provider "github" {
+  token = var.github_token
+}
+
+resource "github_repository" "repo-terraform" {
+  name        = "repo-terraform"
+  description = "Repositorio creado para laboratorio de IaC con Terraform."
+  visibility  = "public"
+}
+
+resource "github_branch" "development" {
+  repository = github_repository.sender-repo-tf.name
+  branch     = "development"
+}
+
+resource "github_branch" "feature" {
+  repository = github_repository.sender-repo-tf.name
+  branch     = "feature"
+}
+
+resource "github_branch" "hotfix" {
+  repository = github_repository.sender-repo-tf.name
+  branch     = "hotfix"
+}
